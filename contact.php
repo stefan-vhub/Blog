9aeef5a1eb95a90219d2c341b282c6fbc8de7341
Petrescu Stefan <64736176+stefan-vhub@users.noreply.github.com>
2020-07-14 19:46:25 +0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,16 +1,6 @@
 <?php 
 include('path.php'); 
 include(ROOT_PATH . "/app/controllers/posts.php");
-
-$to = 'info.stefanpetrescuv.tk';
-$subject = 'This is our test email';
-$message = "<h1>Hi there.</h1><br><p>Thanks for testing!</p>";
-$headers = "From: The Sender Name <sender@johnmorrisonline.com>\r\n";
-$headers .= "Reply-To: replato@johnmorrisonline.com\r\n";
-$headers .= "Content-type: text/html\r\n";
-
-mail($to, $subject, $message, $headers);
-
 ?>
 
 <!DOCTYPE html>
